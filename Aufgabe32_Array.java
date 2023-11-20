c120439e62bb2cbc16439b592ed7a145d540d1b8
YayCover9000 <yassuanf@googlemail.com>
2023-11-20 14:41:33 +0100

    first commit

---
+++ Aufgabe32_Array.java
@@ -0,0 +1,40 @@
+
+public class Aufgabe32_Array {
+	public static void main(String [] args) {
+		int[] varArray;
+		varArray = new int[100];
+		int count = 1;
+		
+//		for (int laufVarE : varArray) {
+//			System.out.print("laufVarE: " + laufVarE + "\t");
+//			System.out.print("VarArray: " + varArray + "\t");
+//			System.out.print("VarArray[e]: " + varArray[laufVarE]);
+//			System.out.println();
+//		}
+		
+		//Array nun mit Werten füllen von 1-100
+		for(int i = 0; i < varArray.length; i++) {
+			varArray[i] = count;
+			count++;
+			
+			//alternativ
+//			varArray[i] = ++i;
+		}
+		
+		// Erneute Ausgabe von Array
+		for (int laufVarE : varArray) {
+			System.out.print("laufVarE: " + laufVarE + "\t");
+			System.out.print("VarArray: " + varArray + "\t");
+			System.out.print("VarArray[laufVarE]: " + varArray[laufVarE-1]); //Array beginnt bei Index 0 dort steht 1
+			System.out.println();
+		}
+		 // Alternative Arrayausgabe
+		for(int i = 0; i<varArray.length; i++) {
+			System.out.print("laufVarE: " + i + "\t");
+			System.out.print("VarArray: " + varArray + "\t");
+			System.out.print("VarArray[laufVarE]: " + varArray[i]); 
+			System.out.println();
+		}
+		
+	}
+}
